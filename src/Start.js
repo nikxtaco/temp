daba9c3c74c6c8e1fae6363df50fdb35581739bd
Sharat Jacob <sharatjacob2@gmail.com>
2023-03-04 01:10:08 +0530

    added static components

--- src/Start.js
+++ src/Start.js
@@ -1,4 +1,8 @@
 import Vector from './Vector.svg';
+import Vector2 from './Vector2.svg';
+import Vector3 from './Vector3.svg';
+import interests from './interests.svg';
+
 
 
 function Start() {
@@ -7,15 +11,21 @@ function Start() {
         <div>
             <div className="Circle Circle1"></div>
             <div className="Circle Circle2"></div>
-            <img src={require("./Sharat.png")} alt="Sharat" width="500px" className="StarterImage" />
+            <img src={require("./Sharat.png")} alt="Sharat" className="StarterImage" />
             <p className="Description">I am a 21-year-old college student pursuing my interests in the fields of UI/UX,
                 content writing, software development, and machine learning, 
                 enthusiastic about building
                 products that make a world of difference.</p>
+            <img src={Vector2} alt="prop" className="Vector2" />
+            <img src={Vector3} alt="prop" className="Vector3" />
             <img src={Vector} alt="prop" className="Vector" />
-            <div className="Circle Circle3"></div>
-            <p className="heading">INTERESTS</p>
-            <p className="subheading">what I do, lol</p>
+
+            <div className="Circle pendulums pendulum3"></div>
+            <div className="Circle pendulums pendulum2"></div>
+            <div className="Circle pendulums pendulum1"></div>
+            <img src={interests} alt="prop" className="interests" />
+
+           
         </div>
     )
 
